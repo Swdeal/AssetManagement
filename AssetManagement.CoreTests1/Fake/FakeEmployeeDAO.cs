1ea747124d09ffdc8700e53be41220e95f40bbab
Primo Guo <primo.guo@hy-techo.com.tw>
2024-03-23 16:05:47 +0800

    新增單元測試

---
+++ AssetManagement.CoreTests1/Fake/FakeEmployeeDAO.cs
@@ -0,0 +1,54 @@
+﻿using AssetManagement.Core.DAO.Interface;
+using AssetManagement.Core.Models.DataObject.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssetManagement.CoreTests.Fake
+{
+    public class FakeEmployeeDAO : IEmployeeDAO
+    {
+        public bool Add(EmployeeDO employeeDO)
+        {
+            if (string.IsNullOrEmpty(employeeDO.EmployeeID) ||
+                string.IsNullOrEmpty(employeeDO.EmployeePWD) ||
+                string.IsNullOrEmpty(employeeDO.EmployeeName) ||
+                string.IsNullOrEmpty(employeeDO.EmployeeEmail))
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public bool Delete(EmployeeDO employeeDO)
+        {
+            if (string.IsNullOrEmpty(employeeDO.EmployeeUID) == false)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool Edit(EmployeeDO employeeDO)
+        {
+            throw new NotImplementedException();
+        }
+
+        public EmployeeDO GetEmployee(string employeeGuid)
+        {
+            return new EmployeeDO
+            {
+                EmployeeUID = employeeGuid,
+            };
+        }
+
+        public List<EmployeeDO> GetEmployeeList(EmployeeQueryDO queryDO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ AssetManagement.CoreTests1/Service/Implement/Employee/EmployeeServiceTests.cs
@@ -0,0 +1,99 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AssetManagement.Core.Service.Implement.Employee;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AssetManagement.Core.Service.Interface;
+using AssetManagement.Core.Models.BusinessObject.Emploee;
+using AssetManagement.Core.DAO.Interface;
+using AssetManagement.CoreTests.Fake;
+using System.ComponentModel;
+using AssetManagement.Core.Models.DataObject.Employee;
+
+namespace AssetManagement.Core.Service.Implement.Employee.Test
+{
+    [TestClass()]
+    public class EmployeeServiceTests
+    {
+        private IEmployeeService _employeeService;
+        private IEmployeeDAO _employeeDAO;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _employeeDAO = new FakeEmployeeDAO();
+            _employeeService = new EmployeeService(_employeeDAO);
+        }
+
+        [TestMethod()]
+        [DisplayName("測試新增失敗")]
+        public void AddTest_False()
+        {
+            // Arrange
+            EmployeeBO employeeBO = new EmployeeBO();
+
+
+            // Act
+            var act = _employeeService.Add(employeeBO);
+
+
+            // Assert
+            Assert.IsFalse(act);
+        }
+
+        [TestMethod()]
+        [DisplayName("測試新增成功")]
+        public void AddTest_True()
+        {
+            // Arrange
+            EmployeeBO employeeBO = new EmployeeBO
+            {
+                EmployeeID = "Test",
+                EmployeePWD = "1234",
+                EmployeeName = "Test",
+                EmployeeEmail = "Test@gmail.com"
+            };
+
+            // Act
+            var act = _employeeService.Add(employeeBO);
+
+
+            // Assert
+            Assert.IsTrue(act);
+        }
+
+
+        [TestMethod()]
+        [DisplayName("測試刪除失敗")]
+        public void DeleteTest_False()
+        {
+            // Arrange
+            string employeeGuid = string.Empty;
+
+
+            // Act
+            var act = _employeeService.Delete(employeeGuid);
+
+
+            // Assert
+            Assert.IsFalse(act);
+        }
+
+        [TestMethod()]
+        [DisplayName("測試刪除成功")]
+        public void DeleteTest_True()
+        {
+            // Arrange
+            string employeeGuid = Guid.NewGuid().ToString();
+
+            // Act
+            var act = _employeeService.Delete(employeeGuid);
+
+
+            // Assert
+            Assert.IsTrue(act);
+        }
+    }
+}
